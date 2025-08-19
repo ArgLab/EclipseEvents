4085a5b61e5c1609c25341105d35515b7872e70e
saminur <saminur.islam08@gmail.com>
2025-08-18 22:48:41 -0400

    correct identification of mouseclick and popup winoow and handling excepiton of sudden close of IDE.

--- EclipseMonitorModules/com.arglab.eclipsedatacollector.core/src/com/arglab/eclipsedatacollector/core/eclipsemonitor/handlers/listeners/MouseClickListener.java
+++ EclipseMonitorModules/com.arglab.eclipsedatacollector.core/src/com/arglab/eclipsedatacollector/core/eclipsemonitor/handlers/listeners/MouseClickListener.java
@@ -9,8 +9,12 @@ import java.util.Map;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -82,63 +86,149 @@ public class MouseClickListener implements MouseListener, Listener{
 
 	@Override
 	public void handleEvent(Event event) {
-		// TODO Auto-generated method stub
-		System.out.println("Mouse");
-		keyBoardClickListener.immediateSave();
-		if(window.getActivePage()!=null) {
-			if(window.getActivePage().getActivePart()!=null) {
-				try {
-					window.getWorkbench().getDisplay().timerExec(200, new Runnable() {
-						
-						@Override
-						public void run() {
-							// TODO Auto-generated method stub			
-							if(window.getActivePage().getActivePart().getTitle()!=null) {
-								
-								IWorkbenchPage page = window.getActivePage(); //Get active page
-								int lineNumber = 0;
-								int columnNumber = 0;
-										
-							    if (page != null) {
-							        IEditorPart editorPart = page.getActiveEditor(); //Get editor
-							        if (editorPart instanceof ITextEditor) {
-							            ITextEditor textEditor = (ITextEditor) editorPart; //If textEditor then cast it to editorPart
-							            IDocumentProvider docProvider = textEditor.getDocumentProvider(); //Get document provider
-							            IDocument document = docProvider.getDocument(textEditor.getEditorInput()); //Get document
-							            ISelectionProvider selectionProvider = textEditor.getSite().getSelectionProvider(); //Get selection provider
-							            ITextSelection textSelection = (ITextSelection) selectionProvider.getSelection(); //Get text selection
-							            lineNumber = textSelection.getStartLine(); //Get start line
-										try {
-											columnNumber = textSelection.getOffset()-document.getLineInformation(lineNumber).getOffset(); //Get offset by total offset-linenumber's offset
-											System.out.println("Line: " + (lineNumber + 1)); //Print line
-								            System.out.println("Column: " + (columnNumber + 1)); //Print Column number
-										} catch (org.eclipse.jface.text.BadLocationException e) {
-											// TODO Auto-generated catch block
-											e.printStackTrace();
-										}
-							        }
-							    }
-								
-								System.out.println("MouseClick to Window: "+window.getActivePage().getActivePart().getTitle());
-								MouseClickData mcd = new MouseClickData(event.x, event.y, window.getActivePage().getActivePart().getTitle(),lineNumber+1,columnNumber + 1);
-								SequentialEventData sedMC = new SequentialEventData("MouseClickEvent", mcd);
-								mouseClickData.add(sedMC);
-								
-							}
-							
-						}
-					});
-				}catch (Exception e) {
-					// TODO: handle exception
-					e.printStackTrace();
-				}			
-			}
-		}
-//		System.out.println("event button: "+event.button);		
-//		System.out.println("evnt has been handled "+event.x);
-//		System.out.println("evnt has been handled "+event.y);
-		
+	    System.out.println("Mouse");
+	    keyBoardClickListener.immediateSave();
+
+	    final Display display = window.getWorkbench().getDisplay();
+
+	    // Defer to end of current UI cycle so caret/selection has updated
+	    display.asyncExec(() -> {
+	        if (display.isDisposed()) return;
+
+	        String windowTitle = "";
+	        Shell shell = null;
+
+	        // 1) Resolve the owning Shell from the clicked widget (popup or main)
+	        if (event.widget instanceof Control) {
+	            Control c = (Control) event.widget;
+	            if (c != null && !c.isDisposed()) {
+	                shell = c.getShell();
+	            }
+	        }
+	        if (shell == null || shell.isDisposed()) {
+	            shell = display.getActiveShell(); // fallback
+	        }
+	        if (shell != null && !shell.isDisposed()) {
+	            windowTitle = safeShellText(shell);
+	        }
+
+	        // Optional: classify popup vs general window if you want to store/type it
+	        boolean isPopup = false;
+	        if (shell != null && !shell.isDisposed()) {
+	            int style = shell.getStyle();
+	            isPopup = (shell.getParent() != null)
+	                    || ( (style & (SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL | SWT.PRIMARY_MODAL | SWT.SYSTEM_MODAL)) != 0 );
+	        }
+
+	        // 2) Capture editor caret (line/column) if an editor is active
+	        int lineNumber = 0;
+	        int columnNumber = 0;
+	        try {
+	            IWorkbenchPage page = window.getActivePage();
+	            if (page != null) {
+	                IEditorPart editorPart = page.getActiveEditor();
+	                if (editorPart instanceof ITextEditor) {
+	                    ITextEditor textEditor = (ITextEditor) editorPart;
+	                    IDocumentProvider docProvider = textEditor.getDocumentProvider();
+	                    IDocument document = docProvider.getDocument(textEditor.getEditorInput());
+	                    ISelectionProvider selectionProvider = textEditor.getSite().getSelectionProvider();
+	                    ITextSelection textSelection = (ITextSelection) selectionProvider.getSelection();
+	                    lineNumber = textSelection.getStartLine();
+	                    try {
+	                        columnNumber = textSelection.getOffset()
+	                                - document.getLineInformation(lineNumber).getOffset();
+	                    } catch (org.eclipse.jface.text.BadLocationException e) {
+	                        e.printStackTrace();
+	                    }
+	                }
+	            }
+	        } catch (Throwable t) {
+	            t.printStackTrace();
+	        }
+
+	        // 3) Log/store with the shell title (this is the precise popup/dialog title if inside one)
+	        System.out.println("MouseClick window: " + windowTitle + (isPopup ? " [popup]" : ""));
+
+	        MouseClickData mcd = new MouseClickData(
+	                event.x,
+	                event.y,
+	                windowTitle,
+	                lineNumber + 1,
+	                columnNumber + 1
+	        );
+	        SequentialEventData sedMC = new SequentialEventData("MouseClickEvent", mcd);
+	        mouseClickData.add(sedMC);
+	    });
 	}
+
+	/** Safe shell title accessor to avoid SWTExceptions on disposed widgets. */
+	private static String safeShellText(Shell shell) {
+	    try {
+	        return shell.getText();
+	    } catch (Throwable t) {
+	        return "";
+	    }
+	}
+	
+//	@Override
+//	public void handleEvent(Event event) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Mouse");
+//		keyBoardClickListener.immediateSave();
+//		if(window.getActivePage()!=null) {
+//			if(window.getActivePage().getActivePart()!=null) {
+//				try {
+//					window.getWorkbench().getDisplay().timerExec(200, new Runnable() {
+//						
+//						@Override
+//						public void run() {
+//							// TODO Auto-generated method stub			
+//							if(window.getActivePage().getActivePart().getTitle()!=null) {
+//								
+//								IWorkbenchPage page = window.getActivePage(); //Get active page
+//								int lineNumber = 0;
+//								int columnNumber = 0;
+//										
+//							    if (page != null) {
+//							        IEditorPart editorPart = page.getActiveEditor(); //Get editor
+//							        if (editorPart instanceof ITextEditor) {
+//							            ITextEditor textEditor = (ITextEditor) editorPart; //If textEditor then cast it to editorPart
+//							            IDocumentProvider docProvider = textEditor.getDocumentProvider(); //Get document provider
+//							            IDocument document = docProvider.getDocument(textEditor.getEditorInput()); //Get document
+//							            ISelectionProvider selectionProvider = textEditor.getSite().getSelectionProvider(); //Get selection provider
+//							            ITextSelection textSelection = (ITextSelection) selectionProvider.getSelection(); //Get text selection
+//							            lineNumber = textSelection.getStartLine(); //Get start line
+//										try {
+//											columnNumber = textSelection.getOffset()-document.getLineInformation(lineNumber).getOffset(); //Get offset by total offset-linenumber's offset
+//											System.out.println("Line: " + (lineNumber + 1)); //Print line
+//								            System.out.println("Column: " + (columnNumber + 1)); //Print Column number
+//										} catch (org.eclipse.jface.text.BadLocationException e) {
+//											// TODO Auto-generated catch block
+//											e.printStackTrace();
+//										}
+//							        }
+//							    }
+//								
+//								System.out.println("MouseClick to Window: "+window.getActivePage().getActivePart().getTitle());
+//								MouseClickData mcd = new MouseClickData(event.x, event.y, window.getActivePage().getActivePart().getTitle(),lineNumber+1,columnNumber + 1);
+//								SequentialEventData sedMC = new SequentialEventData("MouseClickEvent", mcd);
+//								mouseClickData.add(sedMC);
+//								
+//							}
+//							
+//						}
+//					});
+//				}catch (Exception e) {
+//					// TODO: handle exception
+//					e.printStackTrace();
+//				}			
+//			}
+//		}
+////		System.out.println("event button: "+event.button);		
+////		System.out.println("evnt has been handled "+event.x);
+////		System.out.println("evnt has been handled "+event.y);
+//		
+//	}
 
 	
 	
--- EclipseMonitorModules/com.arglab.eclipsedatacollector.core/src/com/arglab/eclipsedatacollector/core/eclipsemonitor/handlers/listeners/PopupWindowListener.java
+++ EclipseMonitorModules/com.arglab.eclipsedatacollector.core/src/com/arglab/eclipsedatacollector/core/eclipsemonitor/handlers/listeners/PopupWindowListener.java
@@ -1,12 +1,16 @@
 package com.arglab.eclipsedatacollector.core.eclipsemonitor.handlers.listeners;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
 import org.eclipse.swt.custom.CTabFolder;
 import org.eclipse.swt.custom.CTabItem;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
 
 import com.arglab.eclipsedatacollector.core.eclipsemonitor.GlobalVars;
 import com.arglab.eclipsedatacollector.core.eclipsemonitor.model.PopupWindowModel;
@@ -16,70 +20,122 @@ import com.arglab.eclipsedatacollector.core.eclipsemonitor.model.jsonmodel.Seque
 
 public class PopupWindowListener {
 
-	public void trackPopupWindows() {
-	    Display display = Display.getDefault();
+	 private final Display display;
+	    private Listener popupFilter; // kept here; Activator doesn't need to manage it
 
-	    // Schedule the UI operation on the UI thread
-	    display.asyncExec(new Runnable() {
-	        @Override
-	        public void run() {
-	            // Add an SWT.Activate filter to track when a shell is activated
-	            display.addFilter(SWT.Activate, event -> {
-	                if (event.widget instanceof Shell) {
-	                    Shell shell = (Shell) event.widget;
-	                    System.out.println("Popup window: " + shell.getText());
+	    public PopupWindowListener(Display display) {
+	        this.display = display;
+	    }
 
-	                    // Introducing a delay to allow the selection to update
-	                    display.timerExec(200, new Runnable() {
-	                        @Override
-	                        public void run() {
-	                            // Retrieve the tab list
-	                            Control[] tablist = shell.getTabList();
-	                            for (Control control : tablist) {
-	                                if (control instanceof Composite) {
-	                                    // Find the CTabFolder recursively within the Composite
-	                                    CTabFolder tabfolder = findTabFolderRecursively(control);
-	                                    if (tabfolder != null) {
-	                                        // Get the currently active tab
-	                                        CTabItem activeTab = tabfolder.getSelection();
-	                                        if (activeTab != null) {
-	                                            System.out.println("Active Tab name is: " + activeTab.getText());
-	                                            PopupWindowModel pwm = new PopupWindowModel();
-	                                            pwm.setPopupwindow(shell.getText());
-	                                            pwm.setCurrentTab(activeTab.getText());
-	                                            SequentialEventData sed = new SequentialEventData("PopUp Mouse Click",pwm);
-	                                            GlobalVars.listSequentialEvents.add(sed);
-	                                            
-	                                        }
-	                                    }
-	                                }
+	    /** Register filters; safe to call multiple times (no-ops if already started). */
+	    public void startTrackingPopups() {
+	        if (display == null || display.isDisposed() || popupFilter != null) return;
+
+	        display.asyncExec(() -> {
+	            if (display.isDisposed() || popupFilter != null) return;
+
+	            // Cache main shell to exclude it
+	            Shell mainShell = PlatformUI.getWorkbench().getActiveWorkbenchWindow() != null
+	                    ? PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell()
+	                    : null;
+
+	            popupFilter = new Listener() {
+	                @Override public void handleEvent(Event event) {
+	                    if (!(event.widget instanceof Shell)) return;
+	                    final Shell shell = (Shell) event.widget;
+
+	                    if (shell.isDisposed() || !shell.isVisible()) return;
+	                    if (mainShell != null && shell == mainShell) return;
+
+	                    int style = shell.getStyle();
+	                    boolean isModal = (style & (SWT.APPLICATION_MODAL | SWT.PRIMARY_MODAL | SWT.SYSTEM_MODAL)) != 0;
+	                    boolean looksLikeDialog = (style & SWT.DIALOG_TRIM) != 0 || isModal || shell.getParent() != null;
+	                    if (!looksLikeDialog) return;
+
+	                    display.timerExec(50, new Runnable() {
+	                        @Override public void run() {
+	                            if (display.isDisposed() || shell.isDisposed()) return;
+
+	                            String title = safeShellText(shell);
+	                            System.out.println("Popup window: " + title);
+
+	                            // If you need tabs inside this dialog, search within the shell only:
+	                            CTabFolder tabFolder = findTabFolderRecursively(shell);
+	                            String activeTab = null;
+	                            if (tabFolder != null && !tabFolder.isDisposed()
+	                                    && tabFolder.getSelection() != null
+	                                    && !tabFolder.getSelection().isDisposed()) {
+	                                activeTab = tabFolder.getSelection().getText();
+	                                System.out.println("Active Tab name is: " + activeTab);
 	                            }
+
+	                            PopupWindowModel pwm = new PopupWindowModel();
+	                            pwm.setPopupwindow(title);
+	                            pwm.setCurrentTab(activeTab);
+	                            SequentialEventData sed = new SequentialEventData(
+	                                    activeTab != null ? "PopUp Mouse Click" : "PopUp Opened", pwm);
+	                            GlobalVars.listSequentialEvents.add(sed);
 	                        }
 	                    });
-	                }
-	            });
-	        }
-	    });
-	}
 
-	private CTabFolder findTabFolderRecursively(Control parent) {
-	    // Check if this control is a CTabFolder
-	    if (parent instanceof CTabFolder) {
-	        return (CTabFolder) parent;
+	                    shell.addListener(SWT.Dispose, e -> display.timerExec(-1, (Runnable) null));
+	                }
+	            };
+
+	            display.addFilter(SWT.Show, popupFilter);
+	            display.addFilter(SWT.Activate, popupFilter);
+	        });
 	    }
 
-	    // If the control is a Composite, check all its children recursively
-	    if (parent instanceof Composite) {
-	        for (Control child : ((Composite) parent).getChildren()) {
-	            CTabFolder tabFolder = findTabFolderRecursively(child);
-	            if (tabFolder != null) {
-	                return tabFolder;
+	    /** Remove filters; safe to call multiple times. */
+	    public void stopTrackingPopups() {
+	        if (display == null || display.isDisposed()) { popupFilter = null; return; }
+	        if (popupFilter == null) return;
+
+	        display.asyncExec(() -> {
+	            if (!display.isDisposed() && popupFilter != null) {
+	                display.removeFilter(SWT.Show, popupFilter);
+	                display.removeFilter(SWT.Activate, popupFilter);
+	                popupFilter = null;
+	            }
+	        });
+	    }
+
+	    private static String safeShellText(Shell shell) {
+	        try { return shell.getText(); } catch (Throwable t) { return ""; }
+	    }
+
+	    private static CTabFolder findTabFolderRecursively(Control root) {
+	        if (root == null || root.isDisposed()) return null;
+	        if (root instanceof CTabFolder) return (CTabFolder) root;
+	        if (root instanceof Composite) {
+	            for (Control child : ((Composite) root).getChildren()) {
+	                CTabFolder found = findTabFolderRecursively(child);
+	                if (found != null) return found;
 	            }
 	        }
+	        return null;
 	    }
 
-	    // Return null if no CTabFolder is found
-	    return null;
-	}
+
+//	private CTabFolder findTabFolderRecursively(Control parent) {
+//	    // Check if this control is a CTabFolder
+//	    if (parent instanceof CTabFolder) {
+//	        return (CTabFolder) parent;
+//	    }
+//
+//	    // If the control is a Composite, check all its children recursively
+//	    if (parent instanceof Composite) {
+//	        for (Control child : ((Composite) parent).getChildren()) {
+//	            CTabFolder tabFolder = findTabFolderRecursively(child);
+//	            if (tabFolder != null) {
+//	                return tabFolder;
+//	            }
+//	        }
+//	    }
+//
+//	    // Return null if no CTabFolder is found
+//	    return null;
+//	}
 
 }
